5895618282fb153acdfb159389f92b59432bb80f
ebarker1 <ebarker1@guardian.com>
2014-09-24 18:30:50 -0400

    removed __get code

--- Library/person.php
+++ Library/person.php
@@ -39,15 +39,6 @@ class Person {
     }
    
     //methods
-//    public function __set($name, $value) {
-//        $this->data[$name] = $value;
-//    }
-//
-//    public function __get($name) {
-//        if (array_key_exists($name, $this->data)) {
-//            return $this->data[$name];
-//        }
-//    } 
     public function getPersonId() {
         return $this->PersonId;
     }
